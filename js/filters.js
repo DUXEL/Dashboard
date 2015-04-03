807ddaf49287d22b816b2d058eb3a05d00845a11
5up3rD4n1 <superdaniel1920@hotmail.com>
2015-04-03 05:33:37 +0000

    A lot of changes
           - new file css/bootstrap-datetimepicker.min.css
           - modified:   css/filters.css
           - new file:   css/tabs.css
           - new file:   images/icons/expand.png
           - new file:   js/bootstrap-datetimepicker.js
           - new file:   js/bootstrap-datetimepicker.min.js
           - modified:   js/filters.js
           - new file:   js/locale/en-gb.js
           - new file:   js/locale/es.js
           - new file:   js/locale/pt.js
           - new file:   js/moment.js
           - modified:   src/filters.html

--- js/filters.js
+++ js/filters.js
@@ -1,3 +1,43 @@
-$('.datepicker').datepicker({
-    format: 'dd/mm/yyyy'
-})
+var myFunc = function (){
+    var start = $('#start-timepicker2').data("DateTimePicker").date();
+    var finish = $('#finish-timepicker2').data("DateTimePicker").date();
+    if ( start != null && finish != null ){
+
+        var years  = finish.diff(start,'years');
+        var month  = finish.subtract(years,'years').diff(start,'months');
+        var days = finish.subtract(month,'months').diff(start,'days');
+        
+        $('#filter-resume-day').html(days);
+        $('#filter-resume-month').html(month);
+        $('#filter-resume-year').html(years);
+
+    }
+};
+
+$(function () {
+                $('#start-timepicker1').datetimepicker();
+                $('#start-timepicker2').datetimepicker();
+                $('#finish-timepicker1').datetimepicker();
+                $('#finish-timepicker2').datetimepicker();
+    
+
+        $("#start-timepicker1").on("dp.change", function (e) {
+            $('#finish-timepicker1').data("DateTimePicker").minDate(e.date);
+            myFunc();
+        });
+        
+        $("#start-timepicker1").on("dp.change", function (e) {
+            $('#finish-timepicker2').data("DateTimePicker").minDate(e.date);
+            myFunc();
+        });
+    
+        $("#finish-timepicker1").on("dp.change", function (e) {
+            $('#start-timepicker1').data("DateTimePicker").maxDate(e.date);
+            myFunc();
+        });
+        
+        $("#finish-timepicker1").on("dp.change", function (e) {
+            $('#start-timepicker2').data("DateTimePicker").maxDate(e.date);
+            myFunc();
+        });
+});
