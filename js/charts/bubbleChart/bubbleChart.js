b220a67f641eaadd30763571d982cd244b6b4dfe
diugalde <dua.0695@gmail.com>
2015-04-03 06:14:33 -0500

    A lot of changes in the index file

--- js/charts/bubbleChart/bubbleChart.js
+++ js/charts/bubbleChart/bubbleChart.js
@@ -1,8 +1,52 @@
+/**
+ * central-click.js
+ */
+d3.svg.BubbleChart.define("central-click", function (options) {
+  var self = this;
+
+  self.setup = (function (node) {
+    var original = self.setup;
+    return function (node) {
+      var fn = original.apply(this, arguments);
+      self.event.on("click", function(node) {
+      });
+      return fn;
+    };
+  })();
+
+  self.reset = (function (node) {
+    var original = self.reset;
+    return function (node) {
+      var fn = original.apply(this, arguments);
+      node.select("text.central-click").remove();
+      return fn;
+    };
+  })();
+
+  self.moveToCentral = (function (node) {
+    var original = self.moveToCentral;
+    return function (node) {
+      var fn = original.apply(this, arguments);
+      var transition = self.getTransition().centralNode;
+      transition.each("end", function() {
+        node.append("text").classed({"central-click": true})
+          .attr(options.attr)
+          .style(options.style)
+          .attr("x", function (d) {return d.cx;})
+          .attr("y", function (d) {return d.cy;})
+          .text(options.text)
+          .style("opacity", 0).transition().duration(self.getOptions().transitDuration / 2).style("opacity", "0.8");
+      });
+      return fn;
+    };
+  })();
+});
+
 $(document).ready(function () {
   var bubbleChart = new d3.svg.BubbleChart({
     supportResponsive: true,
     //container: => use @default
-    size: 600,
+    size: 900,
     //viewBoxSize: => use @default
     innerRadius: 600 / 3.5,
     //outerRadius: => use @default
@@ -30,7 +74,7 @@ $(document).ready(function () {
       {
         name: "central-click",
         options: {
-          text: "(See more detail)",
+          text: "",
           style: {
             "font-size": "12px",
             "font-style": "italic",
