974434b79751bd529848a8e7546c26974edbd676
diugalde <dua.0695@gmail.com>
2015-05-24 18:07:20 -0500

    Fixing Javascript imports
    - Adding ready function to all scripts.
    - Adding partial for the graphs.

--- SocialDashboard/app/assets/javascripts/charts/popularTermsChart/tags.js
+++ SocialDashboard/app/assets/javascripts/charts/popularTermsChart/tags.js
@@ -1,1 +1,6 @@
-var tags = [{"key": "Cat", "value": 50}, {"key": "fish", "value": 19}, {"key": "things", "value": 18}, {"key": "look", "value": 16}, {"key": "two", "value": 15}, {"key": "like", "value": 14}, {"key": "hat", "value": 14}, {"key": "Oh", "value": 13}, {"key": "mother", "value": 12}, {"key": "One", "value": 12}, {"key": "Now", "value": 12}, {"key": "Thing", "value": 12}, {"key": "house", "value": 10}, {"key": "fun", "value": 9}, {"key": "know", "value": 9}, {"key": "good", "value": 9}, {"key": "saw", "value": 9}, {"key": "bump", "value": 8}, {"key": "hold", "value": 7},{"key": "lit", "value": 1}];
+var ready = function() {
+    var tags = [{"key": "Cat", "value": 50}, {"key": "fish", "value": 19}, {"key": "things", "value": 18}, {"key": "look", "value": 16}, {"key": "two", "value": 15}, {"key": "like", "value": 14}, {"key": "hat", "value": 14}, {"key": "Oh", "value": 13}, {"key": "mother", "value": 12}, {"key": "One", "value": 12}, {"key": "Now", "value": 12}, {"key": "Thing", "value": 12}, {"key": "house", "value": 10}, {"key": "fun", "value": 9}, {"key": "know", "value": 9}, {"key": "good", "value": 9}, {"key": "saw", "value": 9}, {"key": "bump", "value": 8}, {"key": "hold", "value": 7},{"key": "lit", "value": 1}];
+}
+
+$(document).on('page:load', ready);
+$(document).ready(ready);
