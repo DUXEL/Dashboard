6a8373d833733ff7a2948c80e2c550756f3ccf4e
Luis <luis-ex@live.com>
2015-05-27 17:36:43 +0000

    Implementing filter funcionality
    - FilterService class
    - FilterController class
    - Modifying CookieAccesor
    - Adding routes to routes.rb

--- SocialDashboard/app/controllers/filters_controller.rb
+++ SocialDashboard/app/controllers/filters_controller.rb
@@ -1,37 +1,45 @@
 class FiltersController < ApplicationController
 
-  before_action :set_filter_service, only: [:create, :update, :destroy, :edit]
-
-  def new
-    type = params[:type]
-    category = params[:category]
-
-    if category.eql? "phrase"
-      render 'filters/add_phrase_chart' , layout: false
-    elsif category.eql? "sna"
-      render 'filters/add_sna_chart' , layout: false
-    end
-  end
+  before_action :set_filter_service, only: [:create, :update, :destroy, :edit, :test]
 
   def edit
-
+    key = params[:id].to_sym
+    json_object = @filter_service.get_filter(key)
+    render json: json_object.to_json
   end
 
   def create
-
+    @filter_service.add_filter(set_filter)
   end
 
   def update
-
+    @filter_service.update_filter(params[:id], set_filter)
   end
 
   def destroy
-
+    key = params[:id].to_sym
+    @filter_service.remove_filter(key)
+    render json: {"hola":"yico"}
   end
 
   private
     def set_filter_service
-      @filter_service = FilterService.new
+      cookie_accessor = CookieAccessor.new(cookies)
+      @filter_service = FilterService.new(cookie_accessor)
     end
 
+    def set_filter
+      if params[:type] == "sna"
+        filter = SNAFilter.new(params[:social_network], params[:user], params[:depth_level])
+      else
+        param_list = []
+        country_list = params[:countries]
+        country_list.each do |country|
+          c = Country.new(country.name, country.latitude, country.longitud)
+          param_list.push(c)
+        end
+        filter = PhrasesFilter.new(params[:language], param_list, params[:start_time], params[:end_time])
+      end
+      return filter
+    end
 end
--- SocialDashboard/app/data/cookie_accessor.rb
+++ SocialDashboard/app/data/cookie_accessor.rb
@@ -1,23 +1,31 @@
 class CookieAccessor
 
+  def initialize(cookies)
+    @cookies = cookies
+  end
+
   def save(map)
     filters = [:filter1, :filter2, :filter3, :filter4, :filter5, :filter6]
     i = 0
-    while cookies[filters[i]] =! nil
+    while @cookies.has_key?(filters[i])
       i+=1
+      if i > 5
+        return -1
+      end
     end
-    cookies[filters[i]] = map
+    @cookies[filters[i]] = map
+    return filters[i]
   end
 
   def find(key)
-    return cookies[key]
+    return @cookies[key]
   end
 
   def update(key, map)
-    cookies[key] = map
+    @cookies[key] = map
   end
 
   def delete(key)
-    cookies[key] = nil
+    @cookies.delete key
   end
 end
