dea304b07f86ec8ac789a5863b2f0f5b8608566d
Luis <luis-ex@live.com>
2015-05-27 11:11:41 +0000

    Implementing cookie accesor methods (save, find, update, delete)

--- SocialDashboard/app/data/cookie_accessor.rb
+++ SocialDashboard/app/data/cookie_accessor.rb
@@ -1,18 +1,23 @@
 class CookieAccessor
 
   def save(map)
-
+    filters = [:filter1, :filter2, :filter3, :filter4, :filter5, :filter6]
+    i = 0
+    while cookies[filters[i]] =! nil
+      i+=1
+    end
+    cookies[filters[i]] = map
   end
 
   def find(key)
-
+    return cookies[key]
   end
 
   def update(key, map)
-
+    cookies[key] = map
   end
 
   def delete(key)
-
+    cookies[key] = nil
   end
 end
