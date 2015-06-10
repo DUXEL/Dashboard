47c88f246ef85a9102201aa1f427193cb7081797
diugalde <dua.0695@gmail.com>
2015-06-09 23:52:45 -0500

    Fixing data access layer
    - Adding twitter client class.
    - Allowing multiple social networks (using the data_source attribute)

---
+++ SocialDashboard/app/data/twitter_client.rb
@@ -0,0 +1,53 @@
+class TwitterClient
+
+  def initialize
+    initialize_twitter_client
+  end
+
+  def followers(username)
+    @twitter_accessor.followers(username).each.collect do |user|
+      "#{user.screen_name}"
+    end
+  end
+
+  def friends(username)
+    @twitter_accessor.friends(username).each.collect do |user|
+      "#{user.screen_name}"
+    end
+  end
+
+  def trends(country)
+    trends = @twitter_accessor.trends(country.woeid)
+    trends.take(10).collect do |post|
+      "#{post.name}"
+    end
+  end
+
+  def posts(filter)
+    options = Hash.new
+    options[:lang] = filter.language
+    options[:count] = 10
+    q = "since:#{filter.start_date} until:#{filter.end_date}"
+    tweets = Array.new
+    filter.country_list.each do |country|
+      geocode = "#{country.latitude},#{country.longitude},500km"
+      options[:geocode] = geocode
+      var = @twitter_accessor.search(q,options)
+      var.take(1).collect do |tweet|
+        tweets.push("#{tweet.text}")
+      end
+    end
+    tweets
+  end
+
+  private
+
+    def initialize_twitter_client
+      @twitter_accessor = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["consumer_key"]
+        config.consumer_secret     = ENV["consumer_secret"]
+        config.access_token        = ENV["access_token"]
+        config.access_token_secret = ENV["access_token_secret"]
+      end
+    end
+end
